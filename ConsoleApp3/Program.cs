353048ca5a871141f5c94026277ce31a3bfb9ed5
Chamba Darwin <darwinchambaa@gmail.com>
2025-01-08 15:01:32 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int nombre=Console.Read();
+            char letra=(char)nombre;
+            Console.WriteLine(letra);
+        }
+       
+    }
+}
